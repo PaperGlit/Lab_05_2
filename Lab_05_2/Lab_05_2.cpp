b33868c1bcc2813cc8289af0c5efec6568f48f3d
Glit <vpad.lazar.04@gmail.com>
2022-11-04 15:24:09 +0200

    Add project files.

---
+++ Lab_05_2/Lab_05_2.cpp
@@ -0,0 +1,67 @@
+﻿// Lab_05_2.cpp
+// Лазар Владислав
+// Лабораторна робота No 5.2
+// Обчислення суми ряду Тейлора за допомогою функцій
+// Варіант 11
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double S(const double x, const double eps);
+
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "arcth(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+
+	x = xp;
+
+	while (x <= xk && abs(x) > 1)
+	{
+		s = S(x, eps);
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << atanh(1 /x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+
+		x += dx;
+	}
+
+	cout << "-------------------------------------------------" << endl;
+
+	return 0;
+}
+
+double S(const double x, const double eps)
+{
+	int n = 0;
+	double a = 1 / x;
+	double s = a;
+
+	do {
+		n++;
+		double R = (2.0 * n - 1) / (2.0 * n + 1) * (1 / (x * x));
+		a *= R;
+		s += a;
+	} while (abs(a) >= eps);
+
+	return s;
+}
---
+++ UnitTest_Lab_05_2/UnitTest_Lab_05_2.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05_2/Lab_05_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab052
+{
+	TEST_CLASS(UnitTestLab052)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(S(8, 0.0000001), atanh(1 / 8.0), 0.0000001);
+		}
+	};
+}
